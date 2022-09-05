836be65f11807e35a23889e273d405f15192823b
Juan Jose Rojo <jjrojoa@gmail.com>
2022-09-05 00:21:14 -0500

    Reworked styles. Added index page content.

--- index.php
+++ index.php
@@ -12,6 +12,121 @@
 <body>
   <?php include './includes/header.php' ?>
 
+  <main class="home-container">
+    <section id="home-title-container">
+      <h1>¿Qué esperas para disfrutar de tu proxima aventura?</h1>
+      <p>Explora nuestras decenas de opciones de hospedaje y ven a probrar nuestros toures guiados, nuestros platillos de los chef más famosos del país y nuestras increibles vistas no importa donde estés.</p>
+      <a href="#hospedaje-section" id="arrow-down">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-down" width="80" height="80" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <line x1="12" y1="5" x2="12" y2="19" />
+          <line x1="18" y1="13" x2="12" y2="19" />
+          <line x1="6" y1="13" x2="12" y2="19" />
+        </svg>
+      </a>
+    </section>
+
+    <section id="mini-services-container">
+      <h2>Nuestros Servicios</h2>
+      <div id="even-min-cards">
+        <div class="mini-service-card">
+          <img src="https://picsum.photos/1000" alt="">
+          <a href="#hospedaje-section">Hospedaje</a>
+        </div>
+        <div class="mini-service-card">
+          <img src="https://picsum.photos/1000" alt="">
+          <a href="#food-section">Alimentación</a>
+        </div>
+      </div>
+      <div id="odd-mini-cards">
+        <div class="mini-service-card">
+          <img src="https://picsum.photos/1000" alt="">
+          <a href="#guides-section">Toures Guiados</a>
+        </div>
+        <div class="mini-service-card">
+          <img src="https://picsum.photos/1000" alt="">
+          <a href="#transport-section">Transportación</a>
+        </div>
+      </div>
+    </section>
+
+    <section id="hospedaje-section">
+      <h2>Hospedaje</h2>
+      <p>Conoce nuestras mejores ofertas de hospedaje para ti</p>
+      <div id="hospedaje-cards-container">
+        <div class="hospedaje-card">
+          <img src="https://picsum.photos/1000" alt="">
+          <p>Ubicación <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, omnis! Recusandae architecto corporis assumenda fugit ratione, animi reprehenderit, illo ab consequatur distinctio nobis et fuga odio quis vitae alias? Quisquam.</p>
+          <a href="#">Reservar</a>
+        </div>
+        <div class="hospedaje-card">
+          <img src="https://picsum.photos/1000" alt="">
+          <p>Ubicación <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, omnis! Recusandae architecto corporis assumenda fugit ratione, animi reprehenderit, illo ab consequatur distinctio nobis et fuga odio quis vitae alias? Quisquam.</p>
+          <a href="#">Reservar</a>
+        </div>
+        <div class="hospedaje-card">
+          <img src="https://picsum.photos/1000" alt="">
+          <p>Ubicación <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, omnis! Recusandae architecto corporis assumenda fugit ratione, animi reprehenderit, illo ab consequatur distinctio nobis et fuga odio quis vitae alias? Quisquam.</p>
+          <a href="#">Reservar</a>
+        </div>
+      </div>
+    </section>
+
+    <section id="guides-section">
+      <h2>Toures Guidados</h2>
+      <p>Como parte de tu reservación te llevamos a conocer las mejores partes de la ciudad en la que reserves tu hospedaje con el 70% de descuento.</p>
+      <div id="guides-collage">
+        <img src="https://picsum.photos/1000" alt="">
+        <img src="https://picsum.photos/1000" alt="">
+        <img src="https://picsum.photos/1000" alt="">
+        <img src="https://picsum.photos/1000" alt="">
+        <img src="https://picsum.photos/1000" alt="">
+        <img src="https://picsum.photos/1000" alt="">
+        <div id="tour-overlay"><a href="#">Reservar un tour guiado</a></div>
+      </div>
+    </section>
+
+    <section id="food-section">
+      <h2>Alimentación</h2>
+      <p>Prueba nuevos platillos o viejos favoritos que harán derretir tu paladar cocinados por excelentes chefs nacionales</p>
+      <div id="food-cards-container">
+        <div class="food-card">
+          <img src="https://picsum.photos/1000" alt="">
+          <div class="food-overlay">
+            <p>Food name</p>
+          </div>
+        </div>
+        <div class="food-card">
+          <img src="https://picsum.photos/1000" alt="">
+          <div class="food-overlay">
+            <p>Food name</p>
+          </div>
+        </div>
+        <div class="food-card">
+          <img src="https://picsum.photos/1000" alt="">
+          <div class="food-overlay">
+            <p>Food name</p>
+          </div>
+        </div>
+        <div class="food-card">
+          <img src="https://picsum.photos/1000" alt="">
+          <div class="food-overlay">
+            <p>Food name</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="transport-section">
+      <h2>Transportación</h2>
+      <p>Como parte de tu paquete de hospedaje incluimos transportación desde tu lugar de llegada a nuestros hoteles.*</p>
+      <div id="transport-desc-container">
+        <img src="https://picsum.photos/1000" alt="">
+        <p><span>¿Preocuparse por el transporte?</span> Con Hotel Kyoto eso es cosa del pasado.</p>
+      </div>
+      <p class="small-text">*Aplican terminos y condiciones. Disponible en paquetes con un valor total previo a descuentos e impuestos de más de $1000000 (un millón) de pesos.</p>
+    </section>
+  </main>
 
   <script>
     const active = document.getElementById("link-home");
